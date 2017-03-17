b81eec806adb9fb0089befd934d58665557bc9a9
seanyboyswag <2150218@slu.edu.ph>
2017-03-17 10:14:56 +0800

    Added Form with initial functionalities

--- offlineapp/scripts.js
+++ offlineapp/scripts.js
@@ -23,3 +23,30 @@ function getApparatus(activity){
 	xmlhttp.open("GET", url, true);
 	xmlhttp.send();
 }
+function addData() {
+    var x = document.getElementById("groupno").value;
+    var y = document.getElementById("name").value;
+    var letters = '/^[a-zA-Z]+$/';
+    if ((parseInt(x) != (x)) && (y == parseInt(y))) {
+        alert("Wrong Value Entered");
+    } else {
+        var rows = "";
+        var name = document.getElementById("name").value;
+        var groupno = document.getElementById("groupno").value;
+        var instructor = document.getElementById("instructor").value;
+        var apparatus = document.getElementById("apparatus").value;
+
+        rows += "<td>" + name + "</td><td>" + groupno + "</td><td>" + instructor + "</td><td>" + apparatus + "</td>";
+        var tbody = document.querySelector("#list tbody");
+        var tr = document.createElement("tr");
+
+        tr.innerHTML = rows;
+        tbody.appendChild(tr)
+
+        //
+    }
+}
+
+function resetForm() {
+    document.getElementById("person").reset();
+}
