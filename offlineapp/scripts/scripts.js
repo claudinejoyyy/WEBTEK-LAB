b2b32b5a4c282bf87c4700f2cd47800181c1f5d4
seanyboyswag <2150218@slu.edu.ph>
2017-03-21 17:52:50 +0800

    Fixed borrow page of student

--- offlineapp/scripts/scripts.js
+++ offlineapp/scripts/scripts.js
@@ -1,6 +1,6 @@
 function getApparatus(activity){
 	var xmlhttp = new XMLHttpRequest();
-	var url = "activities.json";
+	var url = "../json/activities.json";
 
 	xmlhttp.onreadystatechange = function() {
 	    if (this.readyState == 4 && this.status == 200) {
@@ -110,28 +110,28 @@ else {
 
 //Still in development
 
-// function setManual(manual){
-//     var xmlhttp = new XMLHttpRequest();
-//     var url = manual+".json";
+function setManual(manual){
+    var xmlhttp = new XMLHttpRequest();
+    var url = "../json/"+manual+".json";
 
-//     xmlhttp.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
-//             var arr = JSON.parse(this.responseText);
-//             // displayApparatus(myArr, getApparatus);
-//             var out = "<th>Apparatus</th>";
-//             var i;
-//             var j;
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var arr = JSON.parse(this.responseText);
+            // displayApparatus(myArr, getApparatus);
+            var out = "<th>Apparatus</th>";
+            var i;
+            var j;
 
-//             for(i = 0; i < arr.length; i++) {
-//                 if(arr[i].activity === activity){
-//                 for (j = 0; j < arr[i].apparatus.length; j++) {
-//                         out += '<tr><td>' + arr[i].apparatus[j]+ '</td></tr>'
-//                     }
-//                 }
-//             }
-//                 document.getElementById("apparatus").innerHTML = out;
-//         }
-//     };
-//     xmlhttp.open("GET", url, true);
-//     xmlhttp.send();
-// }
+            for(i = 0; i < arr.length; i++) {
+                if(arr[i].activity === activity){
+                for (j = 0; j < arr[i].apparatus.length; j++) {
+                        out += '<tr><td>' + arr[i].apparatus[j]+ '</td></tr>'
+                    }
+                }
+            }
+                document.getElementById("apparatus").innerHTML = out;
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
