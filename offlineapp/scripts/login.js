4ba7523559ef04c1c793c4f0703037c4d1028924
Claudine Joy D. Ragay <2155912@slu.edu.ph>
2017-03-24 14:28:48 +0800

    Edited login.js and users.json

--- offlineapp/scripts/login.js
+++ offlineapp/scripts/login.js
@@ -1,38 +1,54 @@
-function validate()
+function validate(username, password)
   {
-      if(   document.getElementById("text1").value == "josh"
-         && document.getElementById("text2").value == "jen" )
-      {
-          location.href="../index.html";
+    var xmlhttp = new XMLHttpRequest();
+    var url = "../json/users.json";
+
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var arr = JSON.parse(this.responseText);
+          var flag = new Boolean (false);
+
+
+          for (i = 0; i < arr.length; i++) {
+            if (arr[i].username === username && arr[i].password === password) {
+              flag = true;
+            }
+          }
+
+          if (flag === false) {
+           alert("validation failed");
+          } else {
+            location.href = "../index.html";
+              alert()
+          }
       }
-      else
-      {
-          alert( "validation failed" );
-      }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();    
   }
 
-  // Get the modal
-var modal = document.getElementById('myModal');
+//   // Get the modal
+// var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("BR");
+// // Get the button that opens the modal
+// var btn = document.getElementById("BR");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+// // When the user clicks the button, open the modal 
+// // btn.onclick = function() {
+// //     modal.style.display = "block";
+// // }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
