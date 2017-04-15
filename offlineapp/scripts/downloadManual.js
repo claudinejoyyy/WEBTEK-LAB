029174c04dbe1cbe861b20118ca4878976776b93
joshuaNL360 <joshua360notarte@gmail.com>
2017-04-16 01:00:46 +0800

    added javascript codes to alert the user if manual is already downloaded

--- offlineapp/scripts/downloadManual.js
+++ offlineapp/scripts/downloadManual.js
@@ -8,10 +8,49 @@ function downloadManual(manual) {
 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+<<<<<<< .mine
+            var downloadedManual = JSON.parse(this.responseText);
+
+            // manuals.push(downloadedManual)
+            // localStorage.setItem("manuals", JSON.stringify(downloadedManual))
+            // alert(downloadedManual.manualName+" was downloaded.")
+            // var i;
+            console.log(downloadedManual)
+            if (localStorage.getItem(manual, JSON.stringify(downloadedManual)) == true) {
+                alert('Manual already downloaded!');
+            }
+            else{
+                localStorage.setItem(manual, JSON.stringify(downloadedManual))
+                alert('Manual downloaded!');
+            }
+            var a = JSON.parse(localStorage.getItem(manual));
+            var select = document.getElementById("manual");
+            // select.options[select.options.length] = new Option(a, i);
+            // var manuals = [];
+            var manuals = JSON.parse(localStorage.getItem('manuals'));   
+             if(manuals == null){
+||||||| .r114
+            var downloadedManual = JSON.parse(this.responseText);
+
+            // manuals.push(downloadedManual)
+            // localStorage.setItem("manuals", JSON.stringify(downloadedManual))
+            // alert(downloadedManual.manualName+" was downloaded.")
+            // var i;
+            console.log(downloadedManual)
+            localStorage.setItem(manual, JSON.stringify(downloadedManual))
+            alert('Manual downloaded!');
+            var a = JSON.parse(localStorage.getItem(manual));
+            var select = document.getElementById("manual");
+            // select.options[select.options.length] = new Option(a, i);
+            // var manuals = [];
+            var manuals = JSON.parse(localStorage.getItem('manuals'));   
+             if(manuals == null){
+=======
             var downloadedManual = JSON.parse(this.responseText)
             var manuals = JSON.parse(localStorage.getItem('manuals'))
             var flag = true;
             if(manuals === null){
+>>>>>>> .r120
                 manuals = [];
                 manuals.push(downloadedManual);
                 localStorage.setItem('manuals', JSON.stringify(manuals))
